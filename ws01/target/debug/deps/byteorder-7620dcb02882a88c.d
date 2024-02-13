4fb0997874209b9f8995e3074653993a0b1b528c
Peter Derias <peter.derias@gmail.com>
2024-02-13 20:06:15 +1100

    workshop 1 stuff

---
+++ ws01/target/debug/deps/byteorder-7620dcb02882a88c.d
@@ -0,0 +1,6 @@
+/home/peedee/uni/tutoring/24t1/rust/ws01/target/debug/deps/libbyteorder-7620dcb02882a88c.rmeta: /home/peedee/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs /home/peedee/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs
+
+/home/peedee/uni/tutoring/24t1/rust/ws01/target/debug/deps/byteorder-7620dcb02882a88c.d: /home/peedee/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs /home/peedee/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs
+
+/home/peedee/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs:
+/home/peedee/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/io.rs:
---
+++ ws01/target/debug/deps/ws01-17476653c982e24f.d
@@ -0,0 +1,5 @@
+/home/peedee/uni/tutoring/24t1/rust/ws01/target/debug/deps/libws01-17476653c982e24f.rmeta: src/main.rs
+
+/home/peedee/uni/tutoring/24t1/rust/ws01/target/debug/deps/ws01-17476653c982e24f.d: src/main.rs
+
+src/main.rs:
