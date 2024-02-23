0f3e8b0001c531fb51957d9d76e682033f18d4cd
Peter Derias <peter_derias@hotmail.com>
2024-02-23 17:58:21 +1100

    updated week

--- ws01/src/main.rs
+++ ws01/src/main.rs
@@ -1,5 +1,23 @@
 use bmp::{Image, Pixel};
 
+fn draw_pixel(path: &str) {
+    let mut image = Image::new(100, 100); // this keeps being duplicated!! There must be a better waaaaaaaaaaaaaaay 😭
+    image.set_pixel(50, 50, Pixel::new(255, 255, 255));
+    image.save(path).expect("This should save correctly."); // why am i forced to delegate the task of saving to this function? Surely everyone wants to save their masterpiece 😱
+}
+
+fn draw_nth_rows(path: &str) {
+    let mut image = Image::new(100, 100);
+    for column in 0..100 {
+        if column % 2 == 0 {
+            for row in 0..100 {
+                image.set_pixel(row, column, Pixel::new(0, 255, 0));
+            }
+        }
+    }
+    image.save(path).expect("This should save correctly."); 
+}
+
 fn main() {
     let path = std::env::args().nth(1).expect("You must provide a path.");
     let operation = std::env::args().nth(2).expect("You must provide an operation.");
@@ -11,21 +29,4 @@ fn main() {
     } else {
         eprintln!("The operation {operation} was not recognised!");
     }
-}
-
-fn draw_pixel(path: &str) {
-    let mut image = Image::new(100, 100); // this keeps being duplicated!! There must be a better waaaaaaaaaaaaaaay 😭
-    image.set_pixel(50, 50, Pixel::new(255, 255, 255));
-    image.save(path).expect("This should save correctly."); // why am i forced to delegate the task of saving to this function? Surely everyone wants to save their masterpiece 😱
-}
-
-fn draw_nth_rows(path: &str) {
-    let mut image = Image::new(100, 100);
-    // I wish there was a way to get draw_pixel to help me draw lines. There must be a better waaaaaaaaaaaaaaay 😭
-    for i in 0..50 {
-        for j in 0..100 {
-            image.set_pixel(i * 2, j, Pixel::new(0, 255, 0));
-        }
-    }
-    image.save(path).expect("This should save correctly."); 
 }
--- ws01/tut01.md
+++ ws01/tut01.md
@@ -1,24 +1,16 @@
-# Hi!
-
-I am Peter, your co-tutor! My other co-tutor is not here today, but we got a sub.
-- graduated, working 
-- Did a bit of rust
-- crazy enough to build a webapp with it
-- https://syphon-frontend-ugan2hyxwq-ts.a.run.app/
+# Updates
+- Not much! The stuff for lab 1 is due in week 3. Check the rules on due dates for details.
+- The assignment will come out sometime in week 3.
 
 # Important stuff
-- Rust syntax
-- immutability by default: is this a good idea?
-  - How different languages deal with const
-- Basic types: i64, &str and its friends, -> Struct, Tuple build everything that isnt that
-- no NULL! YAAAAAY
-- no Exceptions! YAAAY (sorta)
-- Writing a program vs writing a robust program
-
-# Workshops
-- a place for us to discuss things
-- We are allowed to get distracted
-- We should be working on the workshop though - lab stuff is if we have time for it
-- Workshop today assumes you have started working on picasso
-
-- We will think about what good patterns are easier in rust vs other languages while running this excersise
+- Different paradigms: using iters vs loops etc
+- collections crate
+  - Vec
+  - Deque
+  - LinkedList
+  - some others...
+- HashMap
+  - Use?
+  - what does it take in?
+- serde -> convert types into a parser
+  - This is cool and kinda rare! There are things that come close, like pydantic
