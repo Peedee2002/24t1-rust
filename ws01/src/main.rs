aa23923c9dc32ef3a9983eb4e6c14f97682fa0e7
Peter Derias <peter.derias@gmail.com>
2024-09-10 22:39:34 +1000

    add a solution which solves messy problems in starter code

--- ws01/src/main.rs
+++ ws01/src/main.rs
@@ -1,21 +1,44 @@
 use bmp::{Image, Pixel};
 
-fn draw_pixel(path: &str) {
-    let mut image = Image::new(100, 100); // this keeps being duplicated!! There must be a better waaaaaaaaaaaaaaay 😭
-    image.set_pixel(50, 50, Pixel::new(255, 255, 255));
-    image.save(path).expect("This should save correctly."); // why am i forced to delegate the task of saving to this function? Surely everyone wants to save their masterpiece 😱
+fn draw_pixel(mut image: Image, x: u32, y: u32, pixel: Pixel) -> Image {
+    // technically a useless function and we should use the method instead
+    // mostly used to show how to compose these together
+    image.set_pixel(x, y, pixel);
+    image
 }
 
-fn draw_nth_rows(path: &str) {
+fn draw_nth_rows(mut image: Image) -> Image {
+    let green = Pixel::new(0, 255, 0);
+    for column in (0..image.get_height()).step_by(2) {
+        for row in 0..image.get_width() {
+            image = draw_pixel(image, row, column, green);
+        }
+    }
+    image
 }
 
 fn main() {
     let path = std::env::args().nth(1).expect("You must provide a path.");
     let operation = std::env::args().nth(2).expect("You must provide an operation.");
+    let size_x = std::env::args().nth(3).unwrap_or("100".to_string());
+    let size_y = std::env::args().nth(4).unwrap_or("100".to_string());
+    let mut image = Image::new(
+        size_x.parse().expect("size_x should be a number"),
+        size_y.parse().expect("size_y should be a number")
+    );
 
-    if operation.as_str() == "pixel" {
-        draw_pixel(path.as_str());
-    } else {
-        eprintln!("The operation {operation} was not recognised!");
-    }
+    image = match operation.as_str() {
+        "pixel" => {
+            draw_pixel(image, 50, 50, Pixel::new(255, 255, 255))
+        },
+        "tutor_exercise" => {
+            draw_nth_rows(image)
+        },
+        // Add more image types here in this way - for bonus points, make an enum
+        _ => {
+            panic!("The operation {operation} was not recognised!");
+        }
+    };
+
+    image.save(path).expect("the path given should be writable to");
 }
